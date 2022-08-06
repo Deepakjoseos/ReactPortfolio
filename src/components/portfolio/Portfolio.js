8b15d5306012498dc7c2143cb3549364882b3177
Deepakjoseos <93700189+Deepakjoseos@users.noreply.github.com>
2022-08-06 17:16:39 +0530

    a

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -18,16 +18,18 @@ const Portfolio = () => {
             <img src={IMG1} alt="Work" />
           </div>
           <h3>This is a Portfolio item title</h3>
-          <a href="https://github.com/" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/Alien_pixels"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
