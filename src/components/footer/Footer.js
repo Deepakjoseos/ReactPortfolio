39eb818ec32358c923870191e81ebf6db62db4a7
Deepakjoseos <93700189+Deepakjoseos@users.noreply.github.com>
2022-08-06 15:29:52 +0530

    a

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,54 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer>
+      <a href="#" className="footer__logo">
+        Copyright@2022
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/">
+          <FaFacebookF />
+        </a>
+        <a href="https://twitter.com/">
+          <IoLogoTwitter />
+        </a>
+        <a href="https://instagram.com/">
+          <FiInstagram />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; DeepakJose@2022. All rights Reserved</small>
+      </div>
+    </footer>
   );
 };
 export default Footer;
