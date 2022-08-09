c0aa9d91c05c4672e7ded8516325572c8591fe4b
Deepakjoseos <93700189+Deepakjoseos@users.noreply.github.com>
2022-08-09 13:06:39 +0530

    a

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,19 @@ import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
-
+import Typed from "react-typed";
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Deepak Jose</h1>
+        <Typed
+          className="Text__type"
+          strings={["Deepak Jose", "A Developer", "A Designer", "A Freelancer"]}
+          typeSpeed={150}
+          backSpeed={100}
+          loop
+        />
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
         <HeaderSocials />
